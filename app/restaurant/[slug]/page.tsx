d83747fc58d9090c569dcfd254940dec08e45edd
weapon-master <weaponmaster1024@gmail.com>
2023-03-07 19:28:43 +0800

    nest layout

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,4 +1,3 @@
-import Header from "./components/Header";
 import Images from "./components/Images";
 import Rating from "./components/Rating";
 import ReservationCard from "./components/ReservationCard";
@@ -9,17 +8,14 @@ import Title from "./components/Title";
 export default function RestaurantDetail() {
   return (
     <>
-      <Header />
-      <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-        <div className="bg-white w-[70%] rounded p-3 shadow">
-          <RestaurantNavbar />
-          <Title />
-          <Rating />
-          <Images />
-          <Reviews />
-        </div>
-        <ReservationCard />
+      <div className="bg-white w-[70%] rounded p-3 shadow">
+        <RestaurantNavbar />
+        <Title />
+        <Rating />
+        <Images />
+        <Reviews />
       </div>
+      <ReservationCard />
     </>
   );
 }
