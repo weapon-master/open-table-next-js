3af8519a96909801cba8b8fbc6650b1d3337065d
weapon-master <weaponmaster1024@gmail.com>
2023-03-09 17:16:57 +0800

    loading

---
+++ app/loading.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import SearchHeader from "./components/SearchHeader";
+
+export default function loading() {
+  return (
+    <main>
+      <SearchHeader />
+      <div className="py-3 px-36 mt-10 gap-8 flex flex-wrap justify-center">
+        {Array.from({ length: 12 }).map((_, i) => (
+          <div
+            className="rounded animate-pulse w-64 h-72 bg-gray-300"
+            key={i}
+          ></div>
+        ))}
+      </div>
+    </main>
+  );
+}
